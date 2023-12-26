6314fa2a9397fbfac2867ac00126c74bbd56514c
khishigee <hishigbayr_3@yahoo.com>
2023-12-26 18:29:20 +0800

    change amount

--- src/api/v1/services/transaction.service.ts
+++ src/api/v1/services/transaction.service.ts
@@ -75,7 +75,7 @@ export class TransactionService {
               status: "PENDING"
             };
 
-            if (tran.amount < 20000) {
+            if (tran.amount < 50000) {
               tranData.description = "Мөнгөн дүн хүрэлцэхгүй байна.";
               tranData.status = "FAILED";
 
